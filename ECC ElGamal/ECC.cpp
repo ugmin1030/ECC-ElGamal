2354f9d84ecb34865da3ca913c5455298ca06471
gmin <gllom7884@gmail.com>
2025-11-22 23:39:04 +0900

    ECC 및 GF(p) 기본 구조 생성
    ECC 계수 a, b와 GF(p)의 소수 p 선언 및 정의 분리.

---
+++ ECC ElGamal/ECC.cpp
@@ -0,0 +1,9 @@
+namespace ECC {
+
+	// 타원 곡선의 계수 a, b
+	long long a = 0;
+	long long b = 0;
+
+	// 유한체의 소수 p
+	unsigned long long p = 0;
+}
---
+++ ECC ElGamal/ECC.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace ECC {
+
+	// 타원 곡선의 계수 a, b
+	extern long long a;
+	extern long long b;
+
+	// 유한체의 소수 p
+	extern unsigned long long p;
+}
