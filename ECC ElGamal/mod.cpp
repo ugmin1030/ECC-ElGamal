804ecdfd7f040e8307355e8e9469fc6ddd324722
gmin <gllom7884@gmail.com>
2025-11-22 23:46:27 +0900

    ECC 상 모듈로 연산 기본 구조 생성
    - modMul: long long의 64비트 간 곱셈을 위해 128비트 변수로 중계하는 함수.
    
    - modPow: modMul을 이용한 고속 지수 연산 함수.
    
    - modInverse: modPow로 페르마 소정리를 활용한 곱셈 역원 함수.

---
+++ ECC ElGamal/mod.cpp
@@ -0,0 +1,54 @@
+#include "mod.h"
+#include "ECC.h"
+#include <intrin.h>
+
+namespace ECC {
+	// 모듈로 연산 함수
+	
+	// 모듈로 상 곱셈을 위한 128비트 곱셈 함수
+	unsigned long long modMul(unsigned long long x, unsigned long long y) {
+
+		// 128비트 중 상위 64비트와 하위 64비트
+		unsigned long long hi;
+		unsigned long long lo = _umul128(x, y, &hi);
+
+		// 128비트 값을 p로 나눈 나머지
+		unsigned long long result;
+
+		// mod p 계산
+		_udiv128(hi, lo, p, &result);
+
+		return result;
+	}
+
+	// 고속 지수 연산
+	unsigned long long modPow(unsigned long long x, unsigned long long n) {
+
+		unsigned long long result = 1;
+
+		while (n > 0) {
+
+			// n의 마지막 비트가 1인 경우
+			if (n & 1) {
+
+				// result = (result * x) % p;
+				result = modMul(result, x);
+			}
+
+			// x = (x * x) % p;
+			x = modMul(x, x);
+
+			// n을 오른쪽으로 1비트 시프트
+			n >>= 1;
+		}
+
+		return result;
+	}
+
+	// 곱셈에 대한 역원
+	unsigned long long modInverse(unsigned long long x) {
+
+		// x^(-1) ≡ x^(p-2) (mod p)
+		return modPow(x, p - 2ULL);
+	}
+}
---
+++ ECC ElGamal/mod.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace ECC {
+	// 모듈로 연산 함수
+
+	// 모듈로 상 곱셈을 위한 128비트 곱셈 함수
+	unsigned long long modMul(unsigned long long x, unsigned long long y);
+	
+	// 고속 지수 연산
+	unsigned long long modPow(unsigned long long x, unsigned long long n);
+
+	// 곱셈에 대한 역원
+	unsigned long long modInverse(unsigned long long x);
+}
