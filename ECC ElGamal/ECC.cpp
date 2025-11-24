b4c20bbdb8ae582ca9171c0c95cc8d57b7f2f4d5
gmin <gllom7884@gmail.com>
2025-11-24 19:28:54 +0900

    타원 곡선 계수 a, b의 범위 확장
    -유한체 상이므로 long long에서 unsigned long long으로 확장.

--- ECC ElGamal/ECC.cpp
+++ ECC ElGamal/ECC.cpp
@@ -1,8 +1,8 @@
 namespace ECC {
 
 	// 타원 곡선의 계수 a, b
-	long long a = 0;
-	long long b = 0;
+	unsigned long long a = 0;
+	unsigned long long b = 0;
 
 	// 유한체의 소수 p
 	unsigned long long p = 0;
--- ECC ElGamal/ECC.h
+++ ECC ElGamal/ECC.h
@@ -3,8 +3,8 @@
 namespace ECC {
 
 	// 타원 곡선의 계수 a, b
-	extern long long a;
-	extern long long b;
+	extern unsigned long long a;
+	extern unsigned long long b;
 
 	// 유한체의 소수 p
 	extern unsigned long long p;
