86697e7318bacd4bad7a16d1d0affc707231be83
gmin <gllom7884@gmail.com>
2025-12-14 01:07:00 +0900

    ECC::Point 생성
    1. Point 클래스 정의 및 좌표계 구현
       - 유한체 연산을 지원하는 ULL(Unsigned Long Long wrapper) 타입을 사용하여 x, y 좌표를 저장하도록 설계
       - 타원 곡선 방정식 y^2 = x^3 + ax + b 위에서의 좌표 유효성을 검증하는 구조 마련
    
    2. 무한 원점(Point at Infinity) 처리
       - 타원 곡선 덧셈군의 항등원인 '무한 원점'을 표현하기 위해 _isInfinity 플래그 도입
       - 불필요한 객체 생성을 막고 유일성을 보장하기 위해 싱글톤 패턴(getInfinity) 적용
       - isOnCurve() 호출 시 무한 원점은 항상 참을 반환하도록 예외 처리
    
    3. 생성자 및 초기화 로직 고도화
       - Point(x, y): 입력된 좌표가 실제 곡선 위에 존재하는지 isOnCurve()로 검증 후 초기화
       - Point(x): x좌표만 주어졌을 때, findY()를 통해 가능한 y값들을 찾고 std::mt19937 난수 생성기를 사용하여 두 해 중 하나를 무작위로 선택하여 할당하는 로직 구현
    
    4. 연산자 오버로딩 설계 (인터페이스 선언)
       - 점 덧셈(+), 뺄셈(-), 스칼라 곱셈(*) 등 주요 군 연산을 위한 연산자 오버로딩 선언
       - 비교 연산자(==, !=) 및 출력 스트림(<<) 오버로딩을 통해 디버깅 및 테스트 편의성 확보

--- ECC ElGamal/ECC/Point/Point.cpp
+++ ECC ElGamal/ECC/Point/Point.cpp
@@ -1,182 +1,52 @@
+#include <iostream>
+#include <random>
 #include "Point.h"
-#include "../Modulo/mod.h"
+#include "../group/ULL.h"
 #include "../ECC.h"
 
 namespace ECC {
 
 	// 유한 점 생성자
-	Point::Point(const long long x, const long long y) {
+	Point::Point(const ULL x) noexcept {
+
+		ULL x_value = x;
+
+		// 가능한 y 값 찾기
+		auto y_values = findY(x_value);
+
+		while (!y_values) {
+			std::cout << "주어진 x 좌표에 대한 유효한 y 값이 없습니다. 다른 x 값을 입력하세요: ";
+			std::cin >> x_value;
+			y_values = findY(x_value);
+		}
+
+		// y 값 무작위 선택
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(0, 1);
+
+		this->x = x_value;
+		this->y = dis(gen) ? y_values->first : y_values->second;
+	}
+
+	Point::Point(const ULL x, const ULL y) noexcept {
 		
 		// 곡선 상에 존재하는지 확인
 		if (isOnCurve(x, y)) {
 			this->x = x;
 			this->y = y;
-
-		} else {
+		} 
+		
+		else {
 			std::cout << "점이 곡선 상에 존재하지 않습니다. 무한 원점 반환." << std::endl;
 			*this = getInfinity();
 		}
 	}
 
-	// 무한 원점 생성자
-	Point::Point() : _isInfinity(true) {}
-
 	// 무한 원점 싱글톤 반환 함수
-	Point& Point::getInfinity() {
+	Point& Point::getInfinity() noexcept {
 		static Point instance;
 		return instance;
 	}
 
-	// 점이 ECC 상에 존재하는지 확인
-	bool Point::isOnCurve(const unsigned long long x, const unsigned long long y){
-
-		// y^2
-		unsigned long long ypow2 = modMul(y, y);
-
-		// x^3
-		unsigned long long xpow3 = modPow(x, 3ULL);
-
-		// ax + b
-		unsigned long long ax_plus_b = modAdd(modMul(a, x), b);
-
-		// y^2 == x^3 + ax + b
-		return ypow2 == modAdd(xpow3, ax_plus_b);
-	}
-
-	bool Point::isOnCurve() const {
-
-		// 무한 원점은 항상 true 반환
-		if (_isInfinity) return true;
-
-		return isOnCurve(this->x, this->y);
-	}
-
-	// 점 비교
-	bool Point::operator==(const Point& other) const {
-		return (this->_isInfinity && other._isInfinity) || (this->x == other.x && this->y == other.y);
-	}
-
-	// 점 반전
-	Point Point::operator-() const {
-		return this->_isInfinity ? getInfinity() : Point(x, p - y);
-	}
-
-	// 점 덧셈
-	Point Point::operator+(const Point& other) const {
-
-		// 항등원인 무한 원점과의 연산 처리
-		if (this->_isInfinity) return other;
-		if (other._isInfinity) return *this;
-
-		// 역원 관계 연산 시 항등원 반환
-		if (*this == -other) return getInfinity();
-
-		// 기울기
-		unsigned long long d = 0;
-
-		// 새로운 좌표
-		unsigned long long newX = 0;
-		unsigned long long newY = 0;
-
-		if (*this == other) {	// 점이 같은 경우
-
-			// 두 점이 모두 y = 0인 경우엔 항등원 반환
-			if (y == 0) return getInfinity();
-			
-			// (3x^2 + a)
-			unsigned long long numerator = modAdd(modMul(3ULL, modMul(x, x)), a);
-
-			// (2y)
-			unsigned long long denominator = modMul(2ULL, y);
-
-			// d = (3x^2 + a) / (2y) mod p
-			d = modDiv(numerator, denominator);
-
-			// newX = d^2 - 2x mod p
-			newX = modSub(modMul(d, d), modMul(2ULL, x));
-
-			// newY = d(x - newX) - y mod p
-			newY = modSub(modMul(d, modSub(x, newX)), y);
-
-		} else {	// 점이 다른 경우
-			
-			// (y2 - y1)
-			unsigned long long numerator = modSub(other.y, y);
-
-			// (x2 - x1)
-			unsigned long long denominator = modSub(other.x, x);
-
-			// d = (y2 - y1) / (x2 - x1) mod p
-			d = modDiv(numerator, denominator);
-
-			// newX = d^2 - x1 - x2 mod p
-			newX = modSub(modSub(modMul(d, d), x), other.x);
-
-			// newY = d(x1 - newX) - y1 mod p
-			newY = modSub(modMul(d, modSub(x, newX)), y);
-		}
-
-		return Point(newX, newY);
-	}
-
-	// 점 뺄셈
-	Point Point::operator-(const Point& other) const {
-		return *this + (-other);
-	}
-
-	// 점 스칼라 곱셈
-	// P * k
-	Point Point::operator*(unsigned long long k) const {
-
-		// 무한 원점과의 연산 처리
-		if (this->_isInfinity) return *this;
-
-		// 점이 y = 0인 경우엔 항등원 반환
-		if (y == 0) return (k & 1) ? *this : getInfinity();
-
-		// k = 0인 경우 무한 원점 반환
-		if (k == 0) return getInfinity();
-
-		// 덧셈을 받을 점은 무한 원점으로 초기화
-		Point result = getInfinity();
-
-		Point x = *this;
-		
-		while (k > 0) {
-
-			// k의 마지막 비트가 1인 경우
-			if (k & 1) {
-
-				// result + P
-				result = result + x;
-			}
-
-			x = x + x;
-
-			k >>= 1;
-		}
-
-		return result;
-	}
-
-	// k * P
-	Point operator*(const unsigned long long k, const Point& P) {
-		return P * k;
-	}
-
-	// 출력
-	std::ostream& operator<<(std::ostream& os, const Point& p) {
-
-		// 무한 원점 출력
-		if (p.isInfinity()) {
-			os << "(Infinity)";
-		}
-
-		// 일반 점 출력
-		else {
-			os << "(" << p.getX() << ", " << p.getY() << ")";
-		}
-
-		return os;
-	}
 }
--- ECC ElGamal/ECC/Point/Point.h
+++ ECC ElGamal/ECC/Point/Point.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <iostream>
+#include <utility>
+#include <optional>
+#include "../ECC.h"
+#include "../group/ULL.h"
 
 namespace ECC{
 
@@ -9,57 +13,98 @@ namespace ECC{
 	private:
 
 		// x, y 좌표
-		unsigned long long x = 0;
-		unsigned long long y = 0;
+		ULL x = 0;
+		ULL y = 0;
 
 		// 무한 원점 여부
 		bool _isInfinity = false;
 		
 		// 무한 원점 생성자
-		Point();
+		Point() noexcept : _isInfinity(true) {}
 	
 	public:
 
 		// x로 가능한 y 값 찾기
-		// static unsigned long long* findY(const long long x);
+		static std::optional<std::pair<ULL, ULL>> findY(const ULL x);
 
 		// 유한 점 생성자
-		// Point(const long long x);
-		Point(const long long x, const long long y);
+		Point(const ULL x) noexcept;
+		Point(const ULL x, const ULL y) noexcept;
 
 		// 무한 원점 싱글톤 반환 함수
-		static Point& getInfinity();
+		static Point& getInfinity() noexcept;
 
 		// 무한 원점 여부 반환
-		inline bool isInfinity() const { return _isInfinity; }
-
-		// 점이 ECC 상에 존재하는지 확인
-		static bool isOnCurve(const unsigned long long x, const unsigned long long y);
-		bool isOnCurve() const;
+		inline bool isInfinity() const noexcept { return _isInfinity; }
 
 		// 좌표 반환
-		inline unsigned long long getX() const { return x; }
-		inline unsigned long long getY() const { return y; }
+		inline ULL getX() const noexcept { return x; }
+		inline ULL getY() const noexcept { return y; }
 
-		// 점 비교
-		bool operator==(const Point& other) const;
+		// ECC 좌항, 우항 반환
+		// y^2
+		static inline ULL getLHS(ULL y) noexcept { return y * y; }
+		inline ULL getLHS() const noexcept { return getLHS(y); }
+
+		// x^3 + ax + b
+		static inline ULL getRHS(ULL x) noexcept { return (x ^ (ULL)3) + ((ULL)a * x) + (ULL)b; }
+		inline ULL getRHS() const noexcept { return getRHS(x); }
 
 		// 점 반전
-		Point operator-() const;
+		Point operator-() const noexcept {
+			return this->_isInfinity ? getInfinity() : Point(x, (ULL)p - y);
+		}
+
+		// 점이 ECC 상에 존재하는지 확인
+		static inline bool isOnCurve(const ULL x, const ULL y) noexcept {
+
+			// y^2 == x^3 + ax + b
+			return getLHS(y) == getRHS(x);
+		}
+
+		inline bool isOnCurve() const noexcept {
+
+			// 무한 원점은 항상 true 반환
+			return _isInfinity ? true : isOnCurve(x, y);
+		}
 
 		// 점 덧셈
-		Point operator+(const Point& other) const;
+		Point operator+(const Point& other) const noexcept;
+
+		inline Point& operator+=(const Point& other) {
+			*this = *this + other;
+			return *this;
+		}
 
 		// 점 뺄셈
-		Point operator-(const Point& other) const;
+		Point operator-(const Point& other) const noexcept;
+
+		inline Point& operator-=(const Point& other) {
+			*this = *this - other;
+			return *this;
+		}
 
 		// 점 스칼라 곱셈
-		Point operator*(const unsigned long long k) const;
+		Point operator*(const ULL other) const noexcept;
+
+		inline Point& operator*=(const ULL k) {
+			*this = *this * k;
+			return *this;
+		}
+
+		// 점 비교
+		inline bool operator==(const Point& other) const noexcept {
+			return (this->_isInfinity && other._isInfinity) || (this->x == other.x && this->y == other.y);
+		}
+
+		inline bool operator!=(const Point& other) const noexcept {
+			return !(*this == other);
+		}
 
 		// 출력
-		friend std::ostream& operator<<(std::ostream& os, const Point& p);
+		friend std::ostream& operator<<(std::ostream& os, const Point& p) noexcept;
 	};
 
 	// k * P
-	Point operator*(const unsigned long long k, const Point& P);
+	Point operator*(const ULL k, const Point& P) noexcept;
 }
