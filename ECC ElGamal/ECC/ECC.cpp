2985b58ccde064a4dfbb757fba246175554eba54
gmin <gllom7884@gmail.com>
2025-12-14 15:21:48 +0900

    ECC 초기화 함수 추가
    - ECC 초기화 여부를 나타내는 isNotInitialized 변수 추가
    
    - ECC 계수 a, b를 입력 initECC 추가
    
    - 디버그용으로 사용중

--- ECC ElGamal/ECC/ECC.cpp
+++ ECC ElGamal/ECC/ECC.cpp
@@ -1,11 +1,25 @@
 #include "ECC.h"
 
-namespace ECC {
+namespace ECC {	
+	//// a, b, p 초기화 함수
+	//void initECC() noexcept {
+	//	std::cout << "타원 곡선의 계수 a, b: ";
+	//	std::cin >> a >> b;
 
+	//	std::cout << "유한체의 소수 p: ";
+	//	std::cin >> p;
+
+	//	isNotInitialized = false;
+	//}
+
+	// <디버그용으로 값을 고정>
 	// 타원 곡선의 계수 a, b
-	unsigned long long a = 0;
-	unsigned long long b = 0;
+	unsigned long long a = 2ULL;
+	unsigned long long b = 3ULL;
 
-	// 유한체의 소수 p
-	unsigned long long p = 0;
+	// 유한체의 소수 p (unsigned long long 기준 최대 소수)
+	unsigned long long p = 18446744073709551557ULL;
+
+	// ECC 초기화 여부
+	bool isNotInitialized = false;
 }
--- ECC ElGamal/ECC/ECC.h
+++ ECC ElGamal/ECC/ECC.h
@@ -8,4 +8,10 @@ namespace ECC {
 
 	// 유한체의 소수 p
 	extern unsigned long long p;
+
+	// ECC 초기화 여부
+	extern bool isNotInitialized;
+
+	//// a, b, p 초기화 함수
+	//void initECC() noexcept;
 }
