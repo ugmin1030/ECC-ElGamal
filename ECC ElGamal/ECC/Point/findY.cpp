942cbb68f5a64da11344c0ecdf40a087ec74ad4c
gmin <gllom7884@gmail.com>
2025-12-14 16:43:26 +0900

    find.cpp 미완성 주석 수정

--- ECC ElGamal/ECC/Point/findY.cpp
+++ ECC ElGamal/ECC/Point/findY.cpp
@@ -9,13 +9,13 @@ namespace ECC {
 	// 2차 비잉여 찾기
 	static ULL findQNR() noexcept {
 
-		// z/p ≡ -1인 z 순서대로 탐색
+		// z/p ≡ -1인 z를 순서대로 탐색
 		for (ULL z = ULL(2), t;; z++ ) {
 
-			// z/p ≡ z^{(p-1)/2} mod p 계산
+			// t = z/p = z^{(p-1)/2} (mod p) 계산
 			t = z ^ ((ULL(p - 1)) >> ULL(1));
 
-			// -1이면 반환
+			// -1이면 z는 2차 비잉여이므로 반환
 			if (t == ULL(p - 1)) return z;
 		}
 	}
@@ -23,7 +23,7 @@ namespace ECC {
 	// Tonelli-Shanks 알고리즘
 	static std::pair<ULL, ULL> Tonelli_Shanks(const ULL n) noexcept {
 
-		// p-1 = q * 2^s 꼴로 분해
+		// p-1 = q * 2^s 꼴로 분해 (q는 홀수)
 		ULL q = ULL(p - 1);
 		ULL s = 0;
 
@@ -33,29 +33,23 @@ namespace ECC {
 			s++;
 		}
 
-		// 2차 비잉여 z 찾기. static으로 한 번만 연산.
+		// 2차 비잉여 z 찾기. static으로 선언하여 한 번만 연산.
 		static ULL z = findQNR();
 
-		// c, r, t, t2i, m 변수 초기화
+		// r, t, c, t2i, m 변수 초기화
 
 		// r: 제곱근 후보
 		// r = n^{(q+1)/2}
-		// 
 		ULL r = n ^ ULL((q + 1) >> 1);
 
 		// t: r이 얼마나 틀렸는지 나타내는 오차. 제곱수가 2의 제곱수일 때 항상 1이 됨.
 		// t = n^q = (r^2)/n
-		// r^2 = n이면 t = 1,
-		// r^2 != n이면 t != 1
+		// t = 1일 때, r^2 = n이므로 r 반환
 		ULL t = n ^ q;
 
 		// c: 제곱해도 쉽게 1이 되지 않는 방향을 제공하는 기준축
-		// c = z^q
-		// 
+		// t를 1로 만드는 데 사용됨.
 		ULL c = z ^ q;
-
-		// t^2^i = 1이 되는 최소 i 찾기
-		ULL t2i = ULL(1);
 
 		// m = s
 		// 최소 i를 찾기 위한 상한
@@ -63,30 +57,34 @@ namespace ECC {
 
 		// while t != 0 and t != 1
 		while (t != ULL(0) && t != ULL(1)) {
-			ULL t2i_next = (t2i * t2i);
+			ULL tt = t * t;
 			ULL i = 1;
-			while (i < m) {
-				if (t2i_next == ULL(1)) break;
 
-				t2i_next *= t2i_next;
+			while (i < m) {
+				tt *= tt;
+				if (tt == ULL(1)) break;
 				i++;
 			}
 
 			// b = c^{2^{m-i-1}}
 			// b^2 = c^{2^{m-i}}
-			ULL b = c ^ (ULL(2) ^ (m - i - 1));
+			ULL b = c;
 
-			// 업데이트
-			// r = (r * b)
+			// 2^{m-i-1} 값이 p보다 커지면 오버플로우 발생하므로 반복문으로 거듭제곱 수행
+			for (ULL j = 0; j < m - i - 1; j++) {
+				b *= b; // 거듭제곱 수행
+			}
+
+			// r <- (r * b)
 			r *= b;
 
-			// c = (b * b)
+			// c <- (b * b)
 			c = (b * b);
 
-			// t = (t * b * b)
+			// t <- (t * b * b)
 			t *= c;
 
-			// m = i
+			// m <- i
 			m = i;
 		}
 
